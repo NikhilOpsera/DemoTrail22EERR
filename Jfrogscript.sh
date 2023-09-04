9f6a67fcf4489434f114c9f7ea091458a3f85f9d
NikhilOpsera <137175449+NikhilOpsera@users.noreply.github.com>
2023-09-04 20:13:36 +0530

    Update Jfrogscript.sh

--- Jfrogscript.sh
+++ Jfrogscript.sh
@@ -8,7 +8,7 @@ cat > jfrog-cli.config.v6 << EOF
       \"password\": \"AKCp8pQm7M9KWuE9prYXJcXcZaM5PXENYD3ccYQcT4U1EWmRfZjNRog4KBstbbf4ZMstT43NX\"
 }
 EOF
-mkdir -p configJfrog.jfrog
-mv jfrog-cli.conf.v6 configJfrog.jfrog
+# mkdir -p configJfrog.jfrog
+# mv jfrog-cli.conf.v6 configJfrog.jfrog
 
 jf scan
